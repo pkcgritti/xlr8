04e83f82050b83de44bc171a297226ed142e19f9
Marcos Cesar Gritti <marcos.gritti@baritecnologia.com.br>
2020-05-06 18:17:27 -0300

    chore: initial release

--- src/xlr8/export.py
+++ src/xlr8/export.py
@@ -1,9 +1,19 @@
-import pandas as pd
+def depth(obj):
+    """Get object depth"""
+    if isinstance(obj, dict):
+        return max([1 + depth(v) for v in obj.values()] or [0])
+    if isinstance(obj, (list, tuple, set)):
+        return max([depth(v) for v in obj] or [0])
+    return 0
 
 
-def _get_depth(dict_array):
-    """Get dict array depth"""
-    pass
+def length(obj):
+    """Get object length"""
+    if isinstance(obj, dict):
+        return sum([length(v) for v in obj.values()] or [0])
+    if isinstance(obj, (list, tuple, set)):
+        return max([length(v) for v in obj] or [0]) * len(obj)
+    return 1
 
 
 def _transform(dict_array):
@@ -11,5 +21,4 @@ def _transform(dict_array):
 
 
 def to_xlsx(dict_array):
-
     pass
--- tests/xlr8/test_export.py
+++ tests/xlr8/test_export.py
@@ -1,6 +1,39 @@
 import pytest
 
-from xlr8.export import _get_depth, _transform
+from xlr8.export import _transform, depth, length
+
+
+@pytest.mark.parametrize(
+    'obj,expected',
+    [(None, 0),
+     ('Something', 0),
+     ({'basic': 1}, 1),
+     ({'basic': {'nested': 4}}, 2),
+     ({'basic': {'nested': {'array': [1, 2, {'inside': 4}]}}}, 4)])
+def test_depth(obj, expected):
+    assert depth(obj) == expected
+
+
+@pytest.mark.parametrize(
+    'obj,expected',
+    [({'name': 'John'}, 1),
+     ({'lead': {'name': 'Josh',
+                'birth': '15-08'}}, 2),
+     ({'lead': {'name': 'Jock',
+                'birth': '15-08',
+                'address': {'street': 'sa',
+                                      'city': 'ctba'}}}, 4),
+     ({'lead': {'name': 'Ben',
+                'phones': [{'number': '1', 'code': 2, 'f': 2},
+                           {'number': '2', 'code': 3}]}}, 7),
+     ({'lead': {'name': 'Ben',
+                'phones': [{'number': '1', 'code': 2},
+                           {'number': '2'}]}}, 5),
+     (1, 1),
+     ([1, 2, 3], 3),
+     ([{'name': 'jonh', 'age': 32}, {}], 4)])
+def test_length(obj, expected):
+    assert length(obj) == expected
 
 
 @pytest.mark.parametrize(
@@ -9,15 +42,5 @@ from xlr8.export import _get_depth, _transform
      ({'basic': 1}, 1),
      ({'basic': {'nested': 4}}, 2),
      ({'basic': {'nested': {'array': [1, 2, {'inside': 4}]}}}, 4)])
-def test_get_depth(example, expected):
-    assert _get_depth(example) == expected
-
-
-@pytest.mark.parametrize(
-    'example,expected',
-    [(None, 0),
-     ({'basic': 1}, 1),
-     ({'basic': {'nested': 4}}, 2),
-     ({'basic': {'nested': {'array': [1, 2, {'inside': 4}]}}}, 4)])
-def test_transform():
+def test_transform(example, expected):
     pass
